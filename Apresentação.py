b459e975d2815f2625b4feafa374be9d96ab8bc6
IsaqueR02 <71456433+IsaqueR02@users.noreply.github.com>
2023-09-04 16:47:52 -0300

    Create Apresentação.py

---
+++ Apresentação.py
@@ -0,0 +1,10 @@
+print('Bom dia')
+print('Meu nome é Pitágoras, sou uma IA respónsavel para cuidar de você')
+nome = input('Qual é seu nome? ')
+print('Bom meu caro, '+ nome)
+dia = input('Como foi o seu dia?')
+idade = int(input('Qual é sua idade? '))
+if idade<=30:
+    print('Você é bem jovem meu caro senhor(a), fico feliz em conhecê-lo')
+else:
+    print('Fico feliz em conhecê-lo, caro senhor(a)')
